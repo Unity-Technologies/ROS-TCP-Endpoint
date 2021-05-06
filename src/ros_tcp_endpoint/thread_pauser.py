13dc7fd84a26487d47db3ee94345a2999c37e63e
LaurieCheers-unity <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-05-06 10:44:38 -0700

    RosConnection 2.0 (#57)

--- src/ros_tcp_endpoint/thread_pauser.py
+++ src/ros_tcp_endpoint/thread_pauser.py
@@ -1,16 +1,15 @@
 import threading
 
-
 class ThreadPauser:
     def __init__(self):
         self.condition = threading.Condition()
         self.result = None
-
+	
     def sleep_until_resumed(self):
-        with self.condition:
-            self.condition.wait()
+        with condition:
+            condition.wait()
 
     def resume_with_result(self, result):
         self.result = result
-        with self.condition:
-            self.condition.notify()
+        with condition:
+            condition.notify()
