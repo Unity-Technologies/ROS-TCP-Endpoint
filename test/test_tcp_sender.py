bc0bd2b73e2ab54a2171d71d4bb22cfd419bcd8a
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-06-21 10:56:28 -0700

    Fix test

--- test/test_tcp_sender.py
+++ test/test_tcp_sender.py
@@ -34,7 +34,7 @@ def test_send_message_should_serialize_message(mock_serialize_msg):
 @mock.patch.object(ros_tcp_endpoint.client.ClientThread, "serialize_message")
 def test_send_unity_service_should_serialize_ros_unity_srv(mock_serialize_msg, mock_thread_pauser):
     sender = ros_tcp_endpoint.tcp_sender.UnityTcpSender()
-    sender.send_unity_service(mock.Mock(), mock.Mock(), mock.Mock())
+    sender.send_unity_service_request(mock.Mock(), mock.Mock(), mock.Mock())
     mock_serialize_msg.assert_called_once()
     # TODO: Test the scenario when the queue is not None
     assert sender.queue == None
