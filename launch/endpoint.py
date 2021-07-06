4cd4746df80207dd80dabbb54a56358a1d72272f
Devin Miller (Unity) <mrpropellers@users.noreply.github.com>
2021-07-06 14:35:09 -0700

    Fixing rosparam names in default endpoint launch file. (#80)
    * Fixing package name in endpoint launch file (#78)
    
    * Fixing package name in endpoint launch file
    
    The endpoint launch file is trying to launch `ros_tcp_endpoint`, but the package has been renamed to just `ros_tcp_endpoint`.  Correcting the typo in the launch file.
    
    * Change github workflow action to pre-commit/action@v2.0.3
    
    * add debugging
    
    * Temporarily disable pre-commit
    
    Co-authored-by: Shuo Diao <shuo@unity3d.com>
    
    * Fixing rosparam names in default endpoint launch file.
    
    Co-authored-by: Shuo Diao <shuo@unity3d.com>

--- launch/endpoint.py
+++ launch/endpoint.py
@@ -1,15 +1,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='ros2_tcp_endpoint',
+            package='ros_tcp_endpoint',
             executable='default_server_endpoint',
             emulate_tty=True,
             parameters=[
-                {'/ROS_IP': '0.0.0.0'},
-                {'/ROS_TCP_PORT': 10000},
+                {'ROS_IP': '0.0.0.0'},
+                {'ROS_TCP_PORT': 10000},
             ],
         ),
     ])
